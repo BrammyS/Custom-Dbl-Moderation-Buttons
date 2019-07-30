cae8f768355c30388340439d0b801bc87c3dff4b
Cooper <43901479+cmakh@users.noreply.github.com>
2019-07-30 00:03:53 -0700

    Update CustomDeclineButtons.user.js

--- CustomDeclineButtons.user.js
+++ CustomDeclineButtons.user.js
@@ -46,7 +46,7 @@
     addButton("%50+ commands not working", "More than 50% of your commands listed on your bots page/help command do not provide a response.");
     addPromptedButton("DM open owner command", "Your bot's owner-only \"~input\" command can be used in DMs by anybody. Please restrict this to bot owners only.", "Enter infringing command's name");
     addPromptedButton("Spam command", "Your bot has a command (~input) which causes it to spam either a text channel or a user's DMs.", "Enter infringing command's name");
-    addPromptedBUtton("API Abuse", "Your bot abuses the Discord API.", "Enter sentence explaining abuse");
+    addPromptedButton("API Abuse", "Your bot abuses the Discord API.", "Enter sentence explaining abuse");
 
     function addButton(buttonName, reason) {
         var button = document.createElement("A");
